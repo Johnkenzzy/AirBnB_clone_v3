03740ec312b75dc003b7f65a503da0b7a5d2699f
johnkenzzy <johnkenumeh@gmail.com>
2025-02-15 17:47:42 +0100

    tasks 2, 4 and 5

---
+++ api/v1/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+App initialzation
+"""
+from flask import Flask, make_response, jsonify
+from models import storage
+
+
+app = Flask(__name__)
+
+from api.v1.views import app_views
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 4o4 error"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the database session after each request"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    import os
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True, debug=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,30 @@
+"""Define the api status view"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+@app_views.route('/status')
+def status():
+    """Returns the status of the api"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Retrieves the number of each objects by type"""
+    objs_by_type = {
+                "amenities": storage.count(Amenity),
+                "cities": storage.count(City),
+                "places": storage.count(Place),
+                "reviews": storage.count(Review),
+                "states": storage.count(State),
+                "users": storage.count(User)
+            }
+    return jsonify(objs_by_type)
