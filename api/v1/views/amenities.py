3e3d12012471c0c9ad05b42e39ce3500022d25ef
johnkenzzy <johnkenumeh@gmail.com>
2025-02-17 03:59:06 +0100

    Implement RESTFul APIs to handle amenity object

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Handles all RESTFul API actions for amenity object"""
+from flask import jsonify, abort, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from datetime import datetime
+
+
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities():
+    """Retrieve the list of all amenity objects"""
+    amenities = storage.all(Amenity).values()
+    amenities_list = [amemity.to_dict() for amenity in amenities]
+    return jsonify(amenities_list)
+
+
+@app_views.route(
+        '/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def get_amenity(amenity_id):
+    """Retrieves an amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route(
+        '/amenity/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Deletes an amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    storage.delete(amenity)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenity():
+    """Creates a new amenity object"""
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+    if 'name' not in data:
+        return jsonify({"error": "Missing name"}), 400
+
+    amenity = Amenity(**data)
+    amenity.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
+
+
+@app_views.route(
+        '/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def put_amenity(amenity_id):
+    """Updates exiting data of an amenity object"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    data = request.get_json(silent=True)
+    if data is None:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    skip = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in skip:
+            setattr(Amenity, key, value)
+    amenity.updated_at = datetime.utcnow()
+    storage.save()
+    return make_response(jsonify(amenity.to_dict()), 200)
