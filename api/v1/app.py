25bd9b84b97a95b9acb63c68a74b118767bfd207
johnkenzzy <johnkenumeh@gmail.com>
2025-02-18 02:59:36 +0100

    Implemented CORS for api routes

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,9 +5,11 @@ App initialzation and set up
 from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
